f3ca898b268cfc49aa1f9a809e7d460ca4ffe58f
dallen <danny@wzukltd.com>
2017-01-17 12:15:23 +0000

    Updated update.sh to merge in version of upstream.

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ while getopts v: option
 do
   case "${option}"
   in
-          v) VERSION=${OPTARG};;
+     v) VERSION=${OPTARG};;
   esac
 done
 
@@ -15,20 +15,24 @@ then
   echo "Error: You must specify a version."
 fi
 
-
-
-echo "Dont get here"
-
-
 # Go to Grafana, which is symlinked to the custom version.
 echo "--- Changing directory to the Grafana go project. ---"
 cd $GOPATH/src/github.com/grafana/grafana
 
-git remote add upstream https://github.com/grafana/grafana.git
-git fetch -t upstream # 504 issue, internet issues?
+# Checkout the branch we're merging updates into.
+git checkout master
+
+# Set (opposed to add) the origin url. Add will error, if it already exists.
+git remote set-url upstream https://github.com/grafana/grafana.git
+
+# Fetch tags from upstream
+git fetch upstream
+git fetch --tags upstream
+
+
 
 # then: (like "git pull" which is fetch + merge)
-git merge upstream/$VERSION master
+git rebase $(git rev-parse $VERSION) master
 
 # or, better, replay your local work on top of the fetched branch
 # like a "git pull --rebase"
